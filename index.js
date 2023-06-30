77e4ade24cb53f586fd7ca8a8ef4b497efcbeeb1
sidee <said.issack@student.moringaschool.com>
2023-06-30 08:46:08 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 /* Your Code Here */
+let createEmployeeRecord = (employeeCard) => {
+    let employee
+    return employee = {
+       firstName: employeeCard[0],
+       familyName: employeeCard[1],
+       title: employeeCard[2],
+       payPerHour: employeeCard[3],
+       timeInEvents: [],
+       timeOutEvents: []
+    }}
+    let createEmployeeRecords = (employeesCardsData) => {
+        return employeesCardsData.map(createEmployeeRecord)
+     }
+     
+     function createTimeInEvent(dateStamp) {
+        let [date, hour] = dateStamp.split(' ');
+        this.timeInEvents.push({
+           type: "TimeIn",
+           hour: parseInt(hour, 10),
+           date
+        })
+        return this
+     }
+     function createTimeOutEvent(dateStamp) {
+        let [date, hour] = dateStamp.split(' ');
+        this.timeOutEvents.push({
+           type: "TimeOut",
+           hour: parseInt(hour, 10),
+           date
+        })
+        return this
+     }
+          function hoursWorkedOnDate(dateOfTheForm) {
+        let clockIn = this.timeInEvents.find((e) => e.date == dateOfTheForm).hour
+        let clockOut = this.timeOutEvents.find((e) => e.date == dateOfTheForm).hour
+        return (clockOut - clockIn)/100
+     }     function wagesEarnedOnDate(dateOfTheForm) {
+        const payPerHour = parseInt(this.payPerHour)
+        const hoursWorked = hoursWorkedOnDate.call(this, dateOfTheForm)
+        return payPerHour * hoursWorked
+     }
+     
+     function findEmployeeByFirstName(AllEmployeeRecords, firstName) {
+        return AllEmployeeRecords.find((e) => {
+            return e.firstName
+        })
+     }
+          function calculatePayroll(allEmployeeRecords) {
+        return allEmployeeRecords.reduce((previoutAmount, employee) => {
+           return previoutAmount + allWagesFor.call(employee)
+       }, 0)
+     }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
